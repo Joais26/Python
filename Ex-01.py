2cf5689badada74dc04ae46bb8562dd564492616
Daiana Barbosa <81197346+Joais26@users.noreply.github.com>
2022-03-27 22:12:06 -0300

    01

--- Ex-01.py
+++ Ex-01.py
@@ -1,10 +1,10 @@
 '''
     Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
 '''
-print('='*35)
+print('='*30)
 vetor = []
 for x in range(1, 6):
     n = int(input(f'Digite {x}º número: '))
     vetor.append(n)
-print('='*35)
+print('='*30)
 print(f'Números digitados foram: {vetor}')
